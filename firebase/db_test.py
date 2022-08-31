657b9f3f03fcd39c507fa290ecdf9ec0315a95a8
kitsuya0828 <kitsuyaazuma@gmail.com>
2022-08-31 11:25:40 +0900

    add database test

---
+++ firebase/db_test.py
@@ -0,0 +1,35 @@
+import datetime
+from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+
+
+def main():
+    # ===================== Firebase =====================================
+    # このPythonファイルと同じ階層に認証ファイルを配置して、ファイル名を格納
+    JSON_PATH = 'authentication.json'   # Notionに貼った
+
+    # Firebase初期化
+    cred = credentials.Certificate(JSON_PATH)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    # ====================================================================
+
+    dt_now = datetime.datetime.now()
+
+    try:
+        # Firestoreのコレクションにアクセス
+        doc_ref = db.collection('test')
+        # Firestoreにドキュメントidを指定しないで１つずつ保存
+        doc_ref.add({
+            'title': "test",
+            'date': str(dt_now),
+        })
+    except Exception as e:
+        print(e)
+
+    print('done')
+
+
+if __name__ == '__main__':
+    main()
