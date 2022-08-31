a10bb59f4670249be89a9d05b0d8ed7fbf004545
kitsuya0828 <kitsuyaazuma@gmail.com>
2022-08-31 22:43:01 +0900

    fix

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def handle_message(event):
     
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text="Yeah!!\n{message}"))        
+    TextSendMessage(text=f"Yeah!!\n{message}"))        
 
 if __name__ == "__main__":
 	app.run()
