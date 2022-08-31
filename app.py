3750eb43fe445f014490e0e2666d38fc2f398167
kitsuya0828 <kitsuyaazuma@gmail.com>
2022-08-31 23:40:41 +0900

    fix path

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def handle_message(event):
     
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=f"Yeah!!\n{message}"))        
+    TextSendMessage(text=f"Yeah!!{message[:5]}"))        
 
 if __name__ == "__main__":
 	app.run()
